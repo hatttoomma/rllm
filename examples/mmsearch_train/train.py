9fce66d704b76d6b849b47402a348a1d90cf51dc
hatttoomma <1053003573@qq.com>
2026-03-22 14:55:06 +0800

    1

--- examples/mmsearch_train/train.py
+++ examples/mmsearch_train/train.py
@@ -3,7 +3,7 @@ import hydra
 from rllm.data.dataset import DatasetRegistry
 from rllm.trainer.agent_trainer import AgentTrainer
 
-from workflow import MMSearchWorkflow
+from .workflow import MMSearchWorkflow
 
 @hydra.main(config_path="pkg://rllm.trainer.config", config_name="agent_ppo_trainer", version_base=None)
 def main(config):
