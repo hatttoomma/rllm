66b0b83369a787d0412e9ed0ebe80d630f308ae5
hatttoomma <1053003573@qq.com>
2026-03-27 14:39:54 +0800

    1

--- examples/mmsearch_train/script.sh
+++ examples/mmsearch_train/script.sh
@@ -39,7 +39,7 @@ python3 -m examples.mmsearch_train.train \
     actor_rollout_ref.rollout.temperature=0.6 \
     actor_rollout_ref.rollout.top_p=0.95 \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.65  \
-    actor_rollout_ref.rollout.n=8 \
+    actor_rollout_ref.rollout.n=1 \
     actor_rollout_ref.rollout.val_kwargs.n=1 \
     actor_rollout_ref.rollout.val_kwargs.temperature=0.6 \
     actor_rollout_ref.rollout.val_kwargs.top_p=0.95 \
