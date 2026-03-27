18f3daa176c29a166f2b7c6dd8ebcc7d87b2b578
hatttoomma <1053003573@qq.com>
2026-03-27 14:59:27 +0800

    1

--- examples/mmsearch_train/script.sh
+++ examples/mmsearch_train/script.sh
@@ -7,8 +7,8 @@ python3 -m examples.mmsearch_train.train \
     algorithm.adv_estimator=grpo \
     data.train_batch_size=${NUM_GPUS} \
     data.val_batch_size=8 \
-    data.max_prompt_length=2048 \
-    data.max_response_length=1024 \
+    data.max_prompt_length=4096 \
+    data.max_response_length=2048 \
     data.train_files="$TRAIN_DATASET" \
     data.val_files="$VAL_DATASET" \
     actor_rollout_ref.model.path=$MODEL_PATH \
@@ -31,14 +31,14 @@ python3 -m examples.mmsearch_train.train \
     actor_rollout_ref.actor.clip_ratio_low=0.2 \
     actor_rollout_ref.actor.clip_ratio_high=0.28 \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
-    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.mode="async" \
     actor_rollout_ref.rollout.enforce_eager=False \
     actor_rollout_ref.rollout.temperature=0.6 \
     actor_rollout_ref.rollout.top_p=0.95 \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.65  \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.6  \
     actor_rollout_ref.rollout.n=1 \
     actor_rollout_ref.rollout.val_kwargs.n=1 \
     actor_rollout_ref.rollout.val_kwargs.temperature=0.6 \
