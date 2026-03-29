410646b183c9192147a8cf5998f01c85e978b3b8
hatttoomma <1053003573@qq.com>
2026-03-30 00:44:54 +0800

    1

--- examples/mmsearch_train/README.md
+++ examples/mmsearch_train/README.md
@@ -2,5 +2,7 @@ A concise training code base for TTRL for deepresearch/search agents
 
 TODO:
 GRPO training on mmsearch(current state)
+change instruct model to thinking model
+fix OOM/ change to 8b model
 custom reward for TTRL
 a data loader or data processor for different benchmarks
--- examples/mmsearch_train/script.sh
+++ examples/mmsearch_train/script.sh
@@ -1,4 +1,4 @@
-MODEL_PATH=${MODEL_PATH:-Qwen/Qwen3-VL-2B-Thinking}
+MODEL_PATH=${MODEL_PATH:-Qwen/Qwen3-VL-2B-Instruct}
 TRAIN_DATASET=/home/qyw/rllm/examples/mmsearch_train/data/mmsearch/end2end_verl.parquet
 VAL_DATASET=/home/qyw/rllm/examples/mmsearch_train/data/mmsearch/end2end_verl.parquet
 NUM_GPUS=${NUM_GPUS:-4}
