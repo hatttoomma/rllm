9dfc8b848293a2c813191553fab2108f9b00ae7f
hatttoomma <1053003573@qq.com>
2026-04-09 01:57:16 +0800

    1

--- examples/mmsearch_train/script/wandb_pass_at1_chart.py
+++ examples/mmsearch_train/script/wandb_pass_at1_chart.py
@@ -13,7 +13,7 @@ import wandb
 STEP_SIZE = 10
 
 # Replace with your y values; x will be 0, STEP_SIZE, 2*STEP_SIZE, ...
-YS: list[float] = [0.25, 0.32, 0.33, 0.28, 0.27, 0.27, 0.28, 0, 0, 0]
+YS: list[float] = [0.13, 0.14, 0.15, 0.15, 0.17, 0.3, 0.25, 0, 0, 0]
 
 wandb.init(project="mmsearch-debug", name="pass-at-1-chart")
 for i, y in enumerate(YS):
